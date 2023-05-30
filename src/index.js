32238c4ea4ad2d8275af45f1929f291963e71bf5
takacsberni <takacsberni@gmail.com>
2023-05-30 12:54:17 +0200

    feat: import Provider, and use it after wrap App with it

--- src/index.js
+++ src/index.js
@@ -5,14 +5,17 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createStore } from 'redux';
 import allReducers from './reducers';
+import { Provider } from 'react-redux';
 
-const store = createStore(allReducers,
+const myStore = createStore(allReducers,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={myStore}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
