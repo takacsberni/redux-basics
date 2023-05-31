9071e8a80fd9e4c14bc150fe868f182f8adabe40
takacsberni <takacsberni@gmail.com>
2023-05-31 10:15:02 +0200

    feat: add actions into a separate file and use dispatch within buttons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from "./actions";
 
 function App() {
   const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state => state.isLogged);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
       <h1> Counter: {counter} </h1>
+      <button onClick={ () => dispatch(increment())}>+</button>
+      <button onClick={ () => dispatch(decrement())}>-</button>
+      {isLogged? <h3>Logged in</h3> : ""}
     </div>
   );
 }
